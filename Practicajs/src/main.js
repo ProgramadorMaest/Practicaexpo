fd78ee0b91cb2d76c82b035dc6f4cfd922cfef80
Diego <diegomanuelmartinezulloa@gmal.com>
2025-02-11 23:01:57 -0400

    Exposicion

---
+++ Practicajs/src/main.js
@@ -0,0 +1,12 @@
+import { taskManager } from "./taskmanager.js";
+
+const taskInput = document.getElementById('taskInput');
+const addTaskButton = document.getElementById('addTaskButton');
+
+addTaskButton.addEventListener('click', () => {
+    const taskDescription = taskInput.value;
+    if (taskDescription) {
+        taskManager.addTask(taskDescription);
+        taskInput.value = '';
+    }
+});
